c516f94a904662199cef2cc1291529b0811f717a
Atilio <atiliogoulart@gmail.com>
2021-06-21 18:59:26 -0300

    Updating API

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'app.managers',
+    'app.players',
+
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
