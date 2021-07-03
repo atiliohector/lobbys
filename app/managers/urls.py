7f474e8f03bbe96063efd523d8d880361885938d
Atilio <atiliogoulart@gmail.com>
2021-07-02 23:01:52 -0300

    1.1

--- app/managers/urls.py
+++ app/managers/urls.py
@@ -5,8 +5,5 @@ from . import views
 urlpatterns = [
 
     path('', views.AllManagers.as_view(), name='all_managers'),
-    path('create/', views.CreateManager.as_view(), name='create_manager'),
-    path('update/<int:id>/', views.UpdateManager.as_view(), name='update_manager'),
-    path('delete/<int:id>/', views.DeleteManager.as_view(), name='delete_manager'),
 
 ]
