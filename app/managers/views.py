fda6203686589d279d15516cbb007e9efcf35b8f
Atilio <atiliogoulart@gmail.com>
2021-07-02 23:04:45 -0300

    1.4

--- app/managers/views.py
+++ app/managers/views.py
@@ -12,3 +12,20 @@ class ManagerEndPoint(APIView):
         managers_serializer = ManagerSerializer(managers, many=True)
         return Response(managers_serializer.data)
 
+    def post(self, request):
+
+        data = {
+
+            'name': request.data.get('name'),
+            'age': request.data.get('age'),
+            'champion_name': request.data.get('champion_name'),
+
+        }
+
+        serializer_manager = ManagerSerializer(data=data)
+
+        if serializer_manager.is_valid():
+            serializer_manager.save()
+            return Response(serializer_manager.data)
+        else:
+            return Response(serializer_manager.errors)
