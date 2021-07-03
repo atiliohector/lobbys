658d468ec3467b66f49df593ee7cfb15cd77b444
Atilio <atiliogoulart@gmail.com>
2021-07-03 13:48:17 -0300

    1.1

--- app/managers/models.py
+++ app/managers/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class ManagerModel(models.Model):
     name = models.CharField('Name', max_length=255, blank=False)
     age = models.PositiveIntegerField('Age', default=18, blank=False)
-    champion_name = models.CharField('Champion Name',max_length=255, default="", blank=False)
-    
+    champion_name = models.CharField('Champion Name',max_length=255, default='', blank=False)
+    champion_type = models.CharField('Champion Type', max_length=255, blank=False)
+
     def __str__(self):
         return self.name
