86325f9b6ad15944de5f3973ac9c1ebbb3b6b7a3
Atilio <atiliogoulart@gmail.com>
2021-07-02 22:53:58 -0300

    1.1

--- app/players/urls.py
+++ app/players/urls.py
@@ -5,8 +5,5 @@ from . import views
 urlpatterns = [
     
     path('', views.AllPlayers.as_view(), name='all_players'),
-    path('create/', views.CreatePlayer.as_view(), name='create_player'),
-    path('update/<int:id>/', views.UpdatePlayer.as_view(), name='update_player'),
-    path('delete/<int:id>/', views.DeletePlayer.as_view(), name='delete_player')
 
 ]
--- app/players/views.py
+++ app/players/views.py
@@ -1,21 +1,16 @@
-from django.db.models import query
-from rest_framework import generics
+from rest_framework.view import APIView
+from rest_framework.response import Response
 
 from .models import PlayerModel
 from .serializer import PlayerSerializer
 
-class AllPlayers(generics.ListAPIView):
-    queryset = PlayerModel.objects.all()
-    serializer_class = PlayerSerializer
 
-class CreatePlayer(generics.CreateAPIView):
-    queryset = PlayerModel.objects.all()
-    serializer_class = PlayerSerializer
+class PlayersEndPoint(APIView):
 
-class UpdatePlayer(generics.UpdateAPIView):
-    queryset = PlayerModel.objects.all()
-    serializer_class = PlayerSerializer
+    def get_player(self, request):
 
-class DeletePlayer(generics.DestroyAPIView):
-    queryset = PlayerModel.objects.all()
-    serializer_class = PlayerSerializer
+        players = PlayerModel.objects.all()
+        player_serializer = PlayerSerializer(players, many=True)
+        return Response(player_serializer.data)
+
+     
