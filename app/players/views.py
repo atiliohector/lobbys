86ee6d979639730052981eb2717b532ac3e15316
Atilio <atiliogoulart@gmail.com>
2021-07-02 22:55:28 -0300

    1.2

--- app/players/views.py
+++ app/players/views.py
@@ -1,3 +1,4 @@
+from app import players
 from rest_framework.view import APIView
 from rest_framework.response import Response
 
@@ -13,4 +14,21 @@ class PlayersEndPoint(APIView):
         player_serializer = PlayerSerializer(players, many=True)
         return Response(player_serializer.data)
 
-     
+    def post_player(self, request):
+
+        data = {
+
+            'name': request.data.get('name'),
+            'age': request.data.get('age'),
+            'position': request.data.get('position'),
+            'guild': request.data.get('guild'),
+
+        } 
+
+        player = PlayerSerializer(data=data)
+
+        if player.is_valid():
+            player.save()
+            return Response(player.data)
+        else:
+            return Response(player.errors)
