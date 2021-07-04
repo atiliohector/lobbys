7f9aad85bf0e50f245879241ce9347edacc1aef7
Atilio <atiliogoulart@gmail.com>
2021-07-04 15:45:21 -0300

    1.4

--- app/players/urls.py
+++ app/players/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     
     path('', views.PlayersEndPoint.as_view(), name='all_players'),
-    path('positions/<str:position>/', views.PlayerByPosition.as_view())
+    path('positions/<str:position>/', views.PlayerByPosition.as_view()),
+    path('one/<int:id>/', views.JustPlayer.as_view())
 
 ]
