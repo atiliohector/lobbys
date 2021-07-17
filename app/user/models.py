4bf8530398b9142acad6214ae9699a70122c7089
Atilio <atiliogoulart@gmail.com>
2021-07-17 06:38:08 -0300

    1.2

--- app/user/models.py
+++ app/user/models.py
@@ -4,7 +4,7 @@ class UserModel(models.Model):
 
     name = models.CharField('Name', max_length=255, blank=False)
     age = models.IntegerField('Age', default=18, blank=False)
-    profession = models.CharField('Profession', max_length=255, blank=False)
+    profession = models.CharField('Profession', max_length=255, blank=True, null=True)
     
 
     def __str__(self):
