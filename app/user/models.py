8ffcffce47f879b5eae5963b9f5f0000f84ea942
Atilio <atiliogoulart@gmail.com>
2021-08-08 08:08:41 -0300

    Add user's app

--- app/user/models.py
+++ app/user/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
-class UserModel(models.Model):
-
-    name = models.CharField('Name', max_length=255, blank=False)
-    age = models.IntegerField('Age', default=18, blank=False)
-    profession = models.CharField('Profession', max_length=255, blank=True, null=True)
-    
-
-    def __str__(self):
-        return self.name
+# Create your models here.
