6a38b85dc0a6008fe065b23af8c326a8d713fadb
Atilio <atiliogoulart@gmail.com>
2021-08-08 08:23:14 -0300

    Add  models

--- app/user/models.py
+++ app/user/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 class UserModel(models.Model):
 
-    name = models.CharField(max_length)
+    name = models.CharField(max_length=255)
+    age = models.IntegerField(default=18)
+    type_user = models.CharField(max_length=255)
+    mode_game = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return self.name    
