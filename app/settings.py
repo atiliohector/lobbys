d90a4f00630c557e6d035f001030a27021f3f6d9
Fedorov <atiliogoulart@gmail.com>
2021-08-26 13:30:06 -0300

    Setup settings.py

--- app/settings.py
+++ app/settings.py
@@ -41,6 +41,22 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 ROOT_URLCONF = 'app.urls'
 
 
@@ -81,6 +97,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
+
+STATIC_URL = '/static/'
 
 TIME_ZONE = 'UTC'
 
