55b6fb2ff834fe357e7b549a8cbfe83a4509c986
Fedorov <atiliogoulart@gmail.com>
2021-09-06 19:14:38 -0300

    Remove channels 2.0

--- app/user/channels.py
+++
@@ -1,7 +0,0 @@
-from channels import Group
-
-def conectando(message):
-    Group('users').add(message.reply_channel)
-
-def desconectando(message):
-    Group('users').discard(message.reply_channel)
